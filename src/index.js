b4d275b83e7e0e6963893d2578604a88f38f7dcf
Heyimgautam <99987033+Heyimgautam@users.noreply.github.com>
2024-01-10 21:23:42 +0530

    done

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {Provider} from 'react-redux'
 import store from './store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+
 export const server = "https://project-mern-shopnow-backend.onrender.com";
 // export const server = "http://localhost:5000";
 
